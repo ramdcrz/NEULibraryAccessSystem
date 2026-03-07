8de15737735f7990e1bf7dfaaefbe570cf000e79
Ramil Deocariza Jr. <ramiljr.deocariza@neu.edu.ph>
2026-03-07 13:46:08 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve 'js

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -36,7 +36,7 @@ import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover
 import { useToast } from '@/hooks/use-toast';
 import { toggleUserBlock } from '@/lib/firebase/firestore';
 import { cn } from '@/lib/utils';
-import jsPDF from 'jsPDF';
+import { jsPDF } from 'jspdf';
 import autoTable from 'jspdf-autotable';
 
 export default function AdminDashboard() {
