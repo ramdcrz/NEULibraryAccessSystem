f39be1ae5be3cf00859f7fcdf486c6319ffc213d
Ramil Deocariza Jr. <ramiljr.deocariza@neu.edu.ph>
2026-03-07 07:41:20 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1754625213-ccf19",
+  "appId": "1:810604865595:web:613152c58b4735f517941e",
+  "apiKey": "AIzaSyBgyAiOClW-xdHt65AzD1J4ck5g5YIw0hY",
+  "authDomain": "studio-1754625213-ccf19.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "810604865595"
+};
